ec480f34b6cf1c6caf91af7d18a2e57f18d8422b
Julio Cezar Sabim <juliocezarsabim@gmail.com>
2021-10-04 16:31:08 -0300

    Fixed the useEffect bug

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -10,16 +10,16 @@ const CountryDetails = ({ match }) => {
             .get(`/alpha/${match.params.id}`)
             .then(response => setDetails(response.data))
             .catch(error => console.log(error))
-    }, [])
+    }, [match.params.id])
 
     return (
         <div>
             <h1>{details?.name}</h1>
-            {details?.nativeName}
-            {details?.population}
-            {details?.region}
-            {details?.subRegion}
-            {details?.capital}
+            <h2>{details?.nativeName}</h2>
+            <h2>{details?.population}</h2>
+            <h2>{details?.region}</h2>
+            <h2>{details?.subRegion}</h2>
+            <h2>{details?.capital}</h2>
         </div>
     )
 }
