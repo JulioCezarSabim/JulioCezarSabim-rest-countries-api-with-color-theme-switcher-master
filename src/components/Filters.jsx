25f4a3916dc72d77e919a7f3a7749189b99a526c
Julio Cezar Sabim <juliocezarsabim@gmail.com>
2021-11-15 11:53:22 -0300

    Styled the Filters selector

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './styles/Filters.css'
+
 export default function Filters({ selectedRegion }) {
 
     function update() {
@@ -11,13 +13,16 @@ export default function Filters({ selectedRegion }) {
     }
 
     return (
-        <select onChange={update}>
-            <option value="all">Filter by Region</option>
-            <option value="africa">Africa</option>
-            <option value="americas">America</option>
-            <option value="asia">Asia</option>
-            <option value="europe">Europe</option>
-            <option value="oceania">Oceania</option>
-        </select>
+        <div className="custom-select">
+            <select onChange={update}>
+                <option value="all">Filter by Region</option>
+                <option value="africa">Africa</option>
+                <option value="americas">America</option>
+                <option value="asia">Asia</option>
+                <option value="europe">Europe</option>
+                <option value="oceania">Oceania</option>
+            </select>
+            <i className="fas fa-chevron-down"></i>
+        </div>
     )
 }
