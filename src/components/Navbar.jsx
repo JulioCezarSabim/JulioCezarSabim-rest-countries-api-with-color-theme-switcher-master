73f5a02254a31abc7e5d4e6c92961d21349df481
= <juliocezarsabim@gmail.com>
2021-12-01 17:39:41 -0300

    The icon of the Dark Mode button now changes whether dark mode is active or not

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,14 +14,29 @@ const Navbar = props => {
     }, [darkmode])
 
     function toggleDarkmode() {
-        darkmode === 'light' ? setDarkmode('dark') : setDarkmode('light')
+        const btn_darkmode = document.getElementsByClassName('btn-darkmode')[0]
+        const icon_darkmode = document.getElementById('icon-darkmode')
+
+        if (darkmode === 'light') {
+            setDarkmode('dark')
+            icon_darkmode.classList.add('far')
+            icon_darkmode.classList.remove('fas')
+        }
+        else {
+            setDarkmode('light')
+            icon_darkmode.classList.remove('far')
+            icon_darkmode.classList.add('fas')
+        }
     }
 
     return (
         <nav>
             <ul>
                 <li>Where in the world?</li>
-                <li><button className="btn-darkmode" onClick={toggleDarkmode}><i className="far fa-moon"></i> Dark Mode</button></li>
+                <li>
+                    <i className="fas fa-moon" id="icon-darkmode"></i>
+                    <button className="btn-darkmode" onClick={toggleDarkmode}>Dark Mode</button>
+                </li>
             </ul>
         </nav>
     )
